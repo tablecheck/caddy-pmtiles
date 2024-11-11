509f03f71885d12a5ba05e16168ab2749db48401
Alexander Nicholson 4584443+DragonStuff@users.noreply.github.com <4584443+DragonStuff@users.noreply.github.com>
2024-11-12 01:56:50 +0900

    feature(caddy): add cors

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,14 @@ echo "Environment variables:"
 echo "MAPS_DOMAIN=${MAPS_DOMAIN}"
 echo "MAPS_PMTILES_LOCATION=${MAPS_PMTILES_LOCATION}"
 echo "MAPS_EMAIL=${MAPS_EMAIL}"
+echo "MAPS_SERVE_DOMAIN=${MAPS_SERVE_DOMAIN}"
 
 # Replace environment variables in Caddyfile
 export MAPS_DOMAIN
 export MAPS_PMTILES_LOCATION
 export MAPS_EMAIL
-envsubst '${MAPS_DOMAIN} ${MAPS_PMTILES_LOCATION} ${MAPS_EMAIL}' < /etc/caddy/Caddyfile.template > /etc/caddy/Caddyfile
+export MAPS_SERVE_DOMAIN
+envsubst '${MAPS_DOMAIN} ${MAPS_PMTILES_LOCATION} ${MAPS_EMAIL} ${MAPS_SERVE_DOMAIN}' < /etc/caddy/Caddyfile.template > /etc/caddy/Caddyfile
 
 echo "Caddyfile:"
 cat /etc/caddy/Caddyfile
